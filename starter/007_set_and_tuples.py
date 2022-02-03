a28604921ae38dea900d54179adfd3016b44c0da
serg-sh <serg.shp@gmail.com>
2022-02-03 16:43:26 +0200

    oo7

---
+++ starter/007_set_and_tuples.py
@@ -0,0 +1,19 @@
+str1 = " hello"
+print(set(str1))
+set1 = {1, 3, 2, 0.5}
+set2 = {3, 0.5, 4}
+print(set1)
+print(set1.intersection(set2))
+print(set1.difference(set2))
+
+a = {3, 5, 11, 7, 4 - 3}
+b = {11, 5, 8, 1, 10, 7}
+print(a.intersection(b))
+print(a.union(b))
+
+bul = True
+for ch in str1:
+    if str1.count(ch) > 1:
+        bul = False
+        break
+print(bul)
