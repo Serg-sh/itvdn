9e40495bf8934e2748809add6c3e4299ea9c6490
serg-sh <serg.shp@gmail.com>
2022-02-09 00:07:51 +0200

    002

---
+++ basic/002_encapsulation.py
@@ -0,0 +1,25 @@
+class User:
+    def __init__(self, name, age):
+        self.name = name
+        self.__age = self.__valid_age(age)
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
+        self.__age = User.__valid_age(age)
+
+    @staticmethod
+    def __valid_age(age):
+        if isinstance(age, int) and age > 0:
+            return age
+        else:
+            return -1
+
+
+user1 = User(name="alex", age=30)
+print(user1.name, user1.age)
+user1.age = "20"
+print(user1.name, user1.age)
