2e74ff5bc3e101fd4d095370b74e3afb1fa4ae8f
serg-sh <serg.shp@gmail.com>
2022-02-10 00:49:47 +0200

    003

--- basic/003_polymorphism.py
+++ basic/003_polymorphism.py
@@ -1,1 +1,26 @@
-а = 1
+class User:
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name
+        self.__user_type = "user"
+
+    @staticmethod
+    def access_database():
+        print("access denied")
+
+
+class SuperUser(User):
+    __user_type = "superuser"
+
+    def access_database(self):
+        print("access granted")
+
+
+user = User(name="user", age=30)
+su = SuperUser(name="su", age=35)
+
+def show_access(obj):
+    obj.access_database()
+
+show_access(user)
+show_access(su)
