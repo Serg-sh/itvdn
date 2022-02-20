1002df8fb0ab9fe8457de31b65e88a4cf11848cc
serg-sh <serg.shp@gmail.com>
2022-02-20 22:42:46 +0200

    006

--- basic/006_data_structures.py
+++ basic/006_data_structures.py
@@ -35,5 +35,15 @@ def get_data_rec(data_dict, keys, index=0):
 print(get_data(user, ["info", "special", "projects", 0, "name"]))
 print(get_data_rec(user, ["info", "special", "projects", 0, "name"]))
 
+str_1 = "1_2,40_5,5_32"
 
+fun = lambda s: int(s.split("_")[0]) + int(s.split("_")[1])
 
+list_1 = [fun(res) for res in str_1.split(",")]
+
+print(list_1)
+
+ls = [[1, 2], [3, 4]]
+list_2 = [x for y in ls for x in y]
+
+print(list_2)
