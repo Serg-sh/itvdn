ba905ca57b095a8da1542dc4c1e4e570f9454663
Serg-sh <serg.shp@gmail.com>
2023-01-15 22:24:44 +0200

    numpy

--- tests/JS/my.js
+++ tests/JS/my.js
@@ -1,33 +1,29 @@
 "use strickt";
 
+ let salaries = {
+     John: 100,
+     Ann: 160,
+     Pete: 130
+ };
 
+ let salaries2 = {
+     John: 100,
+     Ann:  160,
+     Pete: 130,
+     Serg: 200
+ };
 
-
-// let salaries = {
-//     John: 100,
-//     Ann: 160,
-//     Pete: 130
-// };
-
-// let salaries2 = {
-//     John: 100,
-//     Ann:  160,
-//     Pete: 130,
-//     Serg: 200
-
-// };
-
-// let sum;
-// function sum_obj(obj){
-//     let sum = 0;
-//     for(let k in obj){
-//         sum += obj[k];
-//     };
-//     return sum
-// };
-// sum = sum_obj(salaries);
-// console.log(sum);
-// console.log(sum_obj(salaries2));
+ let sum;
+ function sum_obj(obj){
+     let sum = 0;
+     for(let k in obj){
+         sum += obj[k];
+     };
+     return sum
+ };
+ sum = sum_obj(salaries);
+ console.log(sum);
+ console.log(sum_obj(salaries2));
 
 
 let menu = {
